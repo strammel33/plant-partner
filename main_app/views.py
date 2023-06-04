7065063d6d0ea8d83ba48a6cc6809c1054cd160f
Suzanne <trammelsuzanne@gmail.com>
2023-06-04 12:04:21 -0400

    make basic plant index view

--- main_app/views.py
+++ main_app/views.py
@@ -15,7 +15,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('cat-index')
+      return redirect('plant-index')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
